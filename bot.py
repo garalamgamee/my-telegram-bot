f9b9a3d1b1ea38e88882eacd96836580a59c0893
garalamgamee <sahachat.mtk365@gmail.com>
2025-07-18 23:35:38 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,18 @@
+import os
+from telegram.ext import Updater, MessageHandler, Filters
+
+TOKEN = os.getenv("7650315995:AAHU9riaEtk7W0CkejSNMQDijihHrXllAF8")
+
+def handle_message(update, context):
+    user_text = update.message.text
+    update.message.reply_text(f"คุณพิมพ์ว่า: {user_text}")
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
